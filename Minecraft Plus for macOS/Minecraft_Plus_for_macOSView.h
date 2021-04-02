49ced713e4325bb0f311ebc9f629f91d04177b6f
Anthony Li <me@anli.dev>
2021-04-01 23:44:35 -0400

    Initial Commit

---
+++ Minecraft Plus for macOS/Minecraft_Plus_for_macOSView.h
@@ -0,0 +1,12 @@
+//
+//  Minecraft_Plus_for_macOSView.h
+//  Minecraft Plus for macOS
+//
+//  Created by Anthony Li on 4/1/21.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Minecraft_Plus_for_macOSView : ScreenSaverView
+
+@end
---
+++ Minecraft Plus for macOS/Minecraft_Plus_for_macOSView.m
@@ -0,0 +1,51 @@
+//
+//  Minecraft_Plus_for_macOSView.m
+//  Minecraft Plus for macOS
+//
+//  Created by Anthony Li on 4/1/21.
+//
+
+#import "Minecraft_Plus_for_macOSView.h"
+
+@implementation Minecraft_Plus_for_macOSView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
